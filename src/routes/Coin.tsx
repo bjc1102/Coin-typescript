cea14d460defc37488c6092bc9aee92d58dfd222
byung ju choi <bjc30849@gmail.com>
2021-12-31 21:28:15 +0900

    Data

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,18 +1,74 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React, {useEffect, useState} from 'react'
+import styled from 'styled-components';
+import { useLocation, useParams } from 'react-router-dom'
 
 interface Params {
     coinId : string
 }
  
+const Header = styled.header`
+    height: 10vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const Container = styled.div`
+    padding: 0px 20px;
+    max-width: 480px;
+    margin: 0 auto;
+`;
+
+const Title = styled.h1`
+    font-size: 48px;
+    color: ${props => props.theme.accentColor};
+    //theme이라서 error 발생이 없다
+`;
+
+const Loader = styled.span`
+    text-align: center;
+    color: ${(props) => props.theme.textColor};
+    display: block;
+`
+
+interface RouteState {
+    name:string;
+}
+
 
 function Coin() {
-    // const {coinId} = useParams<{coinId:string}>();
+    const [loading, setLoading] = useState(true);
     const {coinId} = useParams<Params>();
+    // const {coinId} = useParams<{coinId:string}>();
+    const { state } = useLocation<RouteState>();
+    const [info, setInfo] = useState({});
+    const [priceInfo, setPriceInfo] = useState({});
+    
+
+    useEffect(() => {
+        (async() => {
+            const infoData = await (
+                await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
+            ).json();
+            const priceData = await (
+                await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
+            ).json();
+            setInfo(infoData);
+            setPriceInfo(priceData);
+        })();
+    }, [])
+
 
     return (
-        <h1>{coinId}</h1>
+        <Container>
+            <Header>
+                <Title>{state?.name || "Loading"}</Title>
+            </Header>
+            {loading ? <Loader>Loading...</Loader> : null}
+        </Container>
     )
 }
+// https://api.coinpaprika.com/v1/coins/btc-bitcoin
+// https://api.coinpaprika.com/v1/tickers/btc-bitcoin
 
 export default Coin
--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom';
 import styled from 'styled-components'
 
 
 const Container = styled.div`
     padding: 0px 20px;
+    max-width: 480px;
+    margin: 0 auto;
 `;
 
 const Header = styled.header`
@@ -17,9 +19,11 @@ const Header = styled.header`
 const Title = styled.h1`
     font-size: 48px;
     color: ${props => props.theme.accentColor};
+    //theme이라서 error 발생이 없다
 `;
 
-const CoinsList = styled.ul``;
+const CoinsList = styled.ul`
+`;
 
 const Coin = styled.li`
     background-color: white;
@@ -29,7 +33,8 @@ const Coin = styled.li`
     a {
         padding: 20px;
         transition: color 0.2s ease-in;
-        display: block;
+        display: flex;
+        align-items: center;
     }
     &:hover {
         a{
@@ -38,47 +43,63 @@ const Coin = styled.li`
     }
 `;
 
-const coins = [
-    {
-        id: "btc-bitcoin",
-        name: "Bitcoin",
-        symbol: "BTC",
-        rank: 1,
-        is_new: false,
-        is_active: true,
-        type: "coin",
-        },
-        {
-        id: "eth-ethereum",
-        name: "Ethereum",
-        symbol: "ETH",
-        rank: 2,
-        is_new: false,
-        is_active: true,
-        type: "coin",
-        },
-        {
-        id: "hex-hex",
-        name: "HEX",
-        symbol: "HEX",
-        rank: 3,
-        is_new: false,
-        is_active: true,
-        type: "token",
-        },
-]
+const Loader = styled.span`
+    text-align: center;
+    color: ${(props) => props.theme.accentColor};
+    display: block;
+`
+const Img = styled.img`
+    width: 35px;
+    height: 35px;
+    margin-right: 10px;
+`;
+
+
+interface CoinInterface {
+    id: string,
+    name: string,
+    symbol: string,
+    rank: number,
+    is_new: boolean,
+    is_active: boolean,
+    type: string,
+}
 
 function Coins() {
+    const [coins, setCoins] = useState<CoinInterface[]>([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(()=> {
+        (async() => {
+            const response = await fetch("https://api.coinpaprika.com/v1/coins")
+            const json = await response.json();
+            setCoins(json.slice(0, 100));
+            setLoading(false);
+        })();
+    }, [])
     return (
         <Container>
             <Header>
                 <Title>코인</Title>
             </Header>
-            <CoinsList>
-                {coins.map((coin) => 
-                <Coin key={coin.id}><Link to={`/${coin.id}`}>{coin.name} &rarr; </Link></Coin>
-                )}
-            </CoinsList>
+            {loading ? <Loader>Loading...</Loader>
+                : <CoinsList>
+                    {coins.map((coin) => 
+                    <Coin key={coin.id}>
+                        <Link to={{
+                            pathname:`/${coin.id}`,
+                            state: {name:coin.name},
+
+                        }}>
+                            <Img
+                                src={`https://cryptoicon-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`}
+                            />
+                            {coin.name}
+                            &rarr; 
+                        </Link>
+                    </Coin>
+                    )}
+            </CoinsList>}
         </Container>
     )
 }
