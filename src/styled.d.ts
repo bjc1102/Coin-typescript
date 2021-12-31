7cdb1d2918c8898c747d96a21ca3d9f912f8c472
byung ju choi <bjc30849@gmail.com>
2021-12-31 11:26:58 +0900

    practice

---
+++ src/styled.d.ts
@@ -0,0 +1,14 @@
+// import original module declarations
+import 'styled-components';
+
+// and extend them!
+declare module 'styled-components' {
+
+  //styled-components의 테마 정의를 확장하는 것
+  //DefaultTheme으로 export
+  export interface DefaultTheme {
+    textColor: string;
+    bgColor: string;
+    accentColor:string;
+  }
+}
---
+++ src/theme.ts
@@ -0,0 +1,8 @@
+import { DefaultTheme } from "styled-components";
+
+//theme파일의 정이는 styled.d.ts와 똑같아야 함
+export const Theme:DefaultTheme = {
+    bgColor:"#2c3e50",
+    textColor:"#f5f6fa",
+    accentColor:"#e67e22"
+};
